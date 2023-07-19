eef5a53ec39d28ec8bb325711b12d4f59292075d
lamine <o.lamine33@gmail.com>
2023-07-19 15:20:22 -0400

    changes to rds.tf

--- rds-db.tf
+++ rds-db.tf
@@ -9,6 +9,7 @@ resource "aws_db_instance" "default" {
   parameter_group_name   = "default.mysql8.0"
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.db_sg.id]
+  db_subnet_group_name = aws_db_subnet_group.this.id 
 }
 
 resource "aws_db_subnet_group" "this" {
